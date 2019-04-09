9e1c511ba781c462a6d4be0c2afc336dd52f58c7
ko4an <eechor1@mail.ru>
2019-04-09 12:26:48 +0300

    first

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <link href="./style/style.css" rel="stylesheet" type="text/css">
+      <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet"> 
+      <script src="js/jquery-3.3.1.min.js"></script>
+      <script src="js/script.js"></script>
+      <title>|_-_-_-_-_-_-_-_-_-_-_-_-_|</title>
+    </head>  
+    <body style="overflow-x: hidden; margin: 0px">
+    <header>
+      <div class="logo"></div>
+      <div class="tag_name">Тренажёрный зал <br>GYM</div>
+      <div class="nav">
+        <ul>
+          <li>Главная</li>
+          <li>Услуги</li>
+          <li>Заявки</li>
+          <li>Обратная связь</li>
+        </ul>
+      </div>
+    </header>
---
+++ main.php
@@ -0,0 +1,50 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+<script>
+  $(function(){
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    slideWidth: 1920,
+    useCSS: true
+  });
+});
+</script>
+    <div class="bxslider">
+      <div><img src="img/first.jpg"></div>
+      <div><img src="img/second.jpg"></div>
+      <div><img src="img/third.jpg"></div>
+    </div>
+<div class="main_usl">
+  <div class="usl">
+    <div class="uslimg" style="background-image: url(img/usl1.JPG)"></div>
+    <div class="usltag">Тренажерный зал</div>
+    <div class="uslmain">Индивидуальная работа с клиентом. Определение цели и составление плана работы.</div>
+  </div>
+  <div class="usl">
+    <div class="uslimg" style="background-image: url(img/usl2.JPG)"></div>
+    <div class="usltag">Кардиозона</div>
+    <div class="uslmain">Работа с различными программами на кардиооборудовании.</div>
+  </div>
+  <div class="usl">
+    <div class="uslimg" style="background-image: url(img/usl3.JPG)"></div>
+    <div class="usltag">Персональный тренинг в тренажерном зале</div>
+    <div class="uslmain">Индивидуальная работа с клиентом. Определение цели и составление плана работы.</div>
+  </div>
+  <div class="usl">
+    <div class="uslimg" style="background-image: url(img/usl4.JPG)"></div>
+    <div class="usltag">Реабилитация тренинг</div>
+    <div class="uslmain">Тренировки, направленные на решение определенных задач. Восстановление после травм, заболеваний. Индивидуальный комплекс лечебных тренировок для тех, у кого имеются сердечно-сосудистые заболевания, остеохондроз, проблемы с осанкой, варикоз, травмы опорно-двигательного аппарата и пр.</div>
+  </div>
+  <div class="usl">
+    <div class="uslimg" style="background-image: url(img/usl5.JPG)"></div>
+    <div class="usltag">Функциональный тренинг</div>
+    <div class="uslmain">Функциональный тренинг универсален, развивает все физические качества человека. Сочетает аэробную и силовую нагрузку, поможет стать выносливее, улучшит координацию движений. В ходе занятия прорабатываются даже глубокие мышцы, которые сложно задействовать во время обычных тренировок.</div>
+  </div>
+  <div class="usl">
+    <div class="uslimg" style="background-image: url(img/usl6.JPG)"></div>
+    <div class="usltag">Худеем вместе ТЗ</div>
+    <div class="uslmain">Единственная в России программа по системному групповому похудению под наблюдением тренеров. Это 10 недель тренировок, поддержка кураторов, лекции по питанию, борьба за главный приз и победа над собой.</div>
+  </div>
+</div>
